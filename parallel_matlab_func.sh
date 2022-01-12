89039336319d0f47bb13107c872608215efb3a98
nissy-shota <shota.nishiyama44@gmail.com>
2022-01-12 10:00:55 +0900

    separeted tab nojvm

--- parallel_matlab_func.sh
+++ parallel_matlab_func.sh
@@ -4,7 +4,11 @@
 TARGET_DIR='.'
 cd $TARGET_DIR
 
+# echo $1
+NUM=$1
 # call matlab
-matlab='/usr/local/MATLAB/R2020b/bin/matlab'
-$matlab -nojvm -r func1
+matlab='/usr/local/MATLAB/R2019a/bin/matlab'
+
+gnome-terminal --tab -t "Tab 1" -- \
+                $matlab -nojvm -r 'func1(2); exit'
 
