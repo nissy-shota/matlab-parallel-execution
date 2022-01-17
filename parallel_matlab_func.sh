6bb76894a51f62076b9f6ec1a70fbe38fe4f6133
nissy-shota <shota.nishiyama44@gmail.com>
2022-01-17 14:54:53 +0900

    fix bug

--- parallel_matlab_func.sh
+++ parallel_matlab_func.sh
@@ -9,6 +9,7 @@ NUM=$1
 # call matlab
 matlab='/usr/local/MATLAB/R2019a/bin/matlab'
 
-gnome-terminal --tab -t "Tab 1" -- \
-                $matlab -nojvm -r 'func1(2); exit'
-
+for i in `seq 1 $NUM`
+do
+    gnome-terminal --tab -t "Tab $i" -- $matlab -nojvm -r 'func1('$i')'
+done
